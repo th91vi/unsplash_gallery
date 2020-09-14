ab00343fe36be74ed06c6d5a8cc7acf913ae5ef3
Thiago Vieira <th91v@outlook.com>
2020-09-14 16:44:13 -0300

    feat: added components Header, ImageGrid

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.css'
+
+const Header = () => {
+  return (
+    <header>
+      Unsplash Gallery
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/ImageGrid/ImageGrid.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+
+import './styles.css'
+
+const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+
+const ImageGrid = () => {
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    getImages()
+  }, [])
+
+  const getImages = async () => {
+    const res = await fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
+
+    const data = await res.json()
+
+    setImages(data)
+  }
+
+  return (
+    <div className="content">
+      <section className="grid">
+        {images.map(image => (
+          <div
+            key={image.id}
+            className={`item item-${Math.ceil(
+              image.height / image.width,
+            )}`}
+          >
+            <img
+              src={image.urls.small}
+              alt={image.user.username}
+            />
+          </div>
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default ImageGrid
